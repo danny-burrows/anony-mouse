b65d6386b310275783af63fb4d4a8ba64f0a1c6b
Danny Burrows <dburrows@altoslabs.com>
2026-03-15 11:45:30 +0000

    changes for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const site = "https://danny-burrows.github.io";
+const base = process.env.NODE_ENV === "production" ? "/anony-mouse" : "/";
+
 export default defineConfig({
-  output: "static"
+  output: "static",
+  site,
+  base
 });
